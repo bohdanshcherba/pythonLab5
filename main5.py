7e5f5c5a5e0f2a2f01c073da5294792dc1b112d7
Bohdan Shcherb <73034554+bohdanshcherba@users.noreply.github.com>
2021-03-29 18:53:05 +0300

    changed

--- main5.py
+++ main5.py
@@ -1,27 +1,32 @@
+import re
+
 def main():
-    file = b"access_log_Jul95"
-    date_start = b"[01/Jul/1995:03:35"
-    date_end = b"[01/Jul/1995:03:55"
-    need_lines = []
-    time = False
+    start_date = r"([a-zA-Z0-9._-]+)( - - )(\[01/Jul/1995:)(03:3[5-9]:[0-9]|03:55:00)([a-zA-Z0-9. _/-]+)]" \
+                 r"(.+) [4-5]{1}[0-9]{1}[0-9]{1} (.+)"
+    after_date = r"([a-zA-Z0-9._-]+)( - - )(\[01/Jul/1995:)(03:[4-5][0-9]:\d{2}|03:35:00)([a-zA-Z0-9. _/-]+)]" \
+                 r"(.+) [4-5]{1}[0-9]{1}[0-9]{1} (.+)"
+    stop_date = r"([a-zA-Z0-9._-]+)( - - )(\[01/Jul/1995:)(03:55:00|03:55:00)([a-zA-Z0-9. _/-]+)]" \
+                r"(.+) [4-5]{1}[0-9]{1}[0-9]{1} (.+)"
 
-    with open(file, 'rb') as f:
-        lines = f.readlines()
+    date1 = re.compile(start_date)
+    date2 = re.compile(stop_date)
+    date3 = re.compile(after_date)
 
-    for l in lines:
-        if date_start in l:
-            time = True
-        if time:
-            need_lines.append(l)
-        if date_end in l:
-            time = False
-            break
+    with open('access_log_Jul95', 'r') as file:
+        found = []
+        for line in file:
+            match = date1.search(line)
+            after_date = date3.search(line)
+            stop_match = date2.search(line)
+            if match:
+                found.append(match)
+            elif after_date:
+                found.append(after_date)
+            if stop_match:
+                break
 
-    for l in need_lines:
-        if b" 404 " in l:
-            print(l.decode())
-
-    f.close()
+        for line in found:
+            print(line.group())
 
 
 if __name__ == '__main__':
